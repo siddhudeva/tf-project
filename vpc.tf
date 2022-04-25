8b4af39830893e0dd23b878d2455505cfefbfc0c
SiddhuDeva <dsmahadeva8@gmail.com>
2022-04-25 21:04:38 +0530

    tf-project

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "tf-project" {
   cidr_block = "10.0.8.0/16"
-  enable_dns_support = yes
-  enable_dns_hostnames = yes
+  enable_dns_support = "true"
+  enable_dns_hostnames = "true"
   tags = {
     Name = "tf-project"
   }
